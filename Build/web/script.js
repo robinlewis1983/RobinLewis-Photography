61d155008b65397e22dac1cfbba300f66cbfd0d9
robinlewis1983 <robinlewis1983@googlemail.com>
2025-01-26 15:31:39 +0000

    Drop drop button error flix

--- Build/web/script.js
+++ Build/web/script.js
@@ -1,18 +1,3 @@
-// document.addEventListener('click', e =>{
-//     const isDropdonwButton = e.target.matches("[data-dropdown-button]")
-//     if (!isDropdonwButton && e.target.closest('[data-dropdown]') != null) return
-
-//     let currentDropdown
-//     if (isDropdonwButton){
-//         currentDropdown = e.target.closest('[data-dropdown]')
-//         currentDropdown.classList.toggle('active')
-//     }
-    
-//     Document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
-//         if(dropdown == currentDropdown) return
-//         dropdown.classlist.remove('active')
-//     })
-// })
 
 document.addEventListener('click', e => {
     const isDropdownButton = e.target.matches("[data-dropdown-button]");
